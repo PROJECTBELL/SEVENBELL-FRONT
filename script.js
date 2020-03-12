a27bdc0466e5ba9314c628d91526375c09cff3e6
unknown <n.lefebvre@ecole-ipssi.net>
2020-03-12 17:01:48 +0100

    submit title and content ok, button to send & get

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
 let postButton = document.getElementById('getPostButton');
 postButton.addEventListener('click', getPost)
 
-function getPost(){
-  var settings = {
-    "url": "http://127.0.0.1:3000/posts",
-    "method": "GET"
-  };
-  $.ajax(settings).done(function (response) {
-    showText(response);
-  });
+function getPost() {
+    var settings = {
+        "url": "http://127.0.0.1:3000/posts",
+        "method": "GET"
+    };
+    $.ajax(settings).done(function(response) {
+        showText(response);
+    });
 }
 
-function showText(response){
-  let textBlock = document.getElementById("textBlock");
-  textBlock.innerHTML = "";
+function showText(response) {
+    console.log(response);
+    let textBlock = document.getElementById("textBlock");
+    textBlock.innerHTML = "";
 
-  for(let i = 0; i < response.length; i++) {
-    let line = document.createElement("LI");
-    let lineText = document.createTextNode(`Titre: ${response[i].title}, Content: ${response[i].content}, Date: ${response[i].created_at}`);
-    line.appendChild(lineText); // <li>Texte...</li>
-    textBlock.appendChild(line);
-  }
+    for (let i = 0; i < response.length; i++) {
+        let line = document.createElement("LI");
+        let lineText = document.createTextNode(`Titre: ${response[i].title}, Content: ${response[i].content}, Date: ${response[i].created_at}`);
+        line.appendChild(lineText); // <li>Texte...</li>
+        textBlock.appendChild(line);
+    }
 }
 
-// POST REQUEST
 
-// var data = {
-//   title: document.getElementById("champsTitre").value,
-//   content: "azertyuiop"
-// };
-//
-// var settings = {
-//   url: "http://127.0.0.1:3000/posts",
-//   method: "POST",
-//   ContentType: "application/json",
-//   data: data
-// };
-//
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
+
+
+let sendButton = document.getElementById('send');
+sendButton.addEventListener('click', sendPost)
+
+function sendPost() {
+    var data = {
+        title: document.getElementById("link").value,
+        content: document.getElementById("title").value
+    };
+
+    var settings = {
+        url: "http://127.0.0.1:3000/posts",
+        method: "POST",
+        ContentType: "application/json",
+        data: data
+    };
+
+    $.ajax(settings).done(function(response) {
+        console.log(response);
+    });
+}
