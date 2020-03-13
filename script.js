ac94406624746d70aec8a9c40d2e006f665d041d
unknown <n.lefebvre@ecole-ipssi.net>
2020-03-13 15:08:30 +0100

    main dev, hypertext link, http regex date regex

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.onload = function() {
+    getPost();
+    showText();
+
+};
+
+
 let postButton = document.getElementById('getPostButton');
 postButton.addEventListener('click', getPost)
 
@@ -11,6 +18,7 @@ function getPost() {
     });
 }
 
+
 function showText(response) {
     console.log(response);
     let textBlock = document.getElementById("textBlock");
@@ -18,13 +26,45 @@ function showText(response) {
 
     for (let i = 0; i < response.length; i++) {
         let line = document.createElement("LI");
+        console.log(response[i].created_at)
         let re = /(.*?(?=T))/
         let result = re.exec(response[i].created_at)
-        let lineText = document.createTextNode(`${response[i].title}, URL: ${response[i].content}, Date: ${result[i]} `);
-        line.appendChild(lineText); // <li>Texte...</li>
+
+
+        let lineText = document.createTextNode(`${response[i].title}     URL:   `);
+
+        let lineText2 = document.createTextNode(`--- ${result[0]}  --- Note :  `);
+        line.appendChild(lineText);
+
+
+        let reyt = /(^(http|https):\/\/)/
+        let match = reyt.test(response[i].content)
+        console.log(match);
+        if (match == false) {
+            response[i].content = `http://${response[i].content}`;
+        }
+        let para = document.createElement("a");
+        let paraline = document.createTextNode(` ${response[i].content}`)
+
+        para.setAttribute("href", `${response[i].content}`);
+        para.appendChild(paraline);
+        line.appendChild(para);
+
+
+        line.appendChild(lineText2);
+
+
+
+        //lineText2.appendChild(lineText); // <li>Texte...</li>
         textBlock.appendChild(line);
+
+
+
+
     }
+
 }
+
 
 
 
@@ -33,6 +73,14 @@ let sendButton = document.getElementById('send');
 sendButton.addEventListener('click', sendPost)
 
 function sendPost() {
+
+    let reyt = /(^(http|https):\/\/)/
+    let match = reyt.test(document.getElementById("link").value)
+    console.log(match);
+    if (match == false) {
+        document.getElementById("link").value = "";
+    }
+
     var data = {
         title: document.getElementById("link").value,
         content: document.getElementById("title").value
